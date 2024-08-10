e5a4bb60aa7140636f8234c7aef3e8dda166b599
hayward2007 <hayward2007@naver.com>
2024-08-10 13:52:40 +0900

    Initial Commit

---
+++ phonch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  phonch
+//
+//  Created by 김형석 on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ phonch/phonchApp.swift
@@ -0,0 +1,17 @@
+//
+//  phonchApp.swift
+//  phonch
+//
+//  Created by 김형석 on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct phonchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
